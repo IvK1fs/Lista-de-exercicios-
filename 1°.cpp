9b4b47f0e0ad591a5be78e084b4790f391799d61
IvK1fs <166817912+IvK1fs@users.noreply.github.com>
2024-05-27 13:28:30 -0300

    Add files via upload

---
+++ 1°.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int x;
+ int A[6] = {1, 0, 5, -2, -5, 7};
+  x = A[0] + A[1] + A[5];
+  cout <<"soma é igual a: " << x <<  endl;
+  A[4] = 100;
+
+  for(int i = 0; i < 6; i++){
+    cout<<"{"<<  A[i] <<"}" << endl;
+  }
+ 
+}
---
+++ 3ª.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+const int tamanho = 10;
+
+void calcularquadrado(int vetorog[], int vetorquadrado[]){
+
+  for (int i =0; i < tamanho; i++){
+
+    vetorquadrado[i] = vetorog[i] * vetorog[i];
+
+  }
+
+}
+void imprimirvetor(int vetor[], int tamanho){
+
+  cout << "[";
+
+  for(int i =0; i < tamanho; i++){
+
+    cout << vetor[i];
+
+  if (i < tamanho -1){
+
+    cout << ", ";  
+      }
+
+}
+
+cout << "]";
+
+}
+int main() {
+
+  int vetorog[tamanho];
+
+  int vetorquadrado[tamanho];
+
+  cout << "digite " << tamanho << " numeros: " << endl;
+
+  for( int i = 0; i < tamanho ; i++){
+
+    cout << "número " << i+ 1 << ": ";
+
+    cin >> vetorog[i];
+
+  }
+
+calcularquadrado( vetorog, vetorquadrado);
+
+  cout <<endl << "original: ";
+
+  imprimirvetor( vetorog,  tamanho);
+
+cout << endl << "vetor quadrado: ";
+  
+imprimirvetor( vetorquadrado,tamanho);
+
+
+
+}
