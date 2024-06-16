df2cedd6baa470d09f778a6c89e18ae391878799
IvK1fs <166817912+IvK1fs@users.noreply.github.com>
2024-06-16 19:09:13 -0300

    Create 42.cpp

---
+++ pasta de arquivos/42.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+const int size =5;
+
+int mt[size][size];
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size ; j++) {
+            mt[i][j] = i * j;
+}
+ }
+
+  
+    cout << "Matriz resultante:" << endl;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            cout << mt[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+
+
+
+    
+}
