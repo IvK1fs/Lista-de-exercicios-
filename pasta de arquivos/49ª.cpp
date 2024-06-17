9492ec4474a97c4a138a9767cb95aa71cafa2e3f
IvK1fs <166817912+IvK1fs@users.noreply.github.com>
2024-06-16 21:27:27 -0300

    Create 49ª.cpp

---
+++ pasta de arquivos/49ª.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+#include <iostream>
+using namespace std;
+
+const int SIZE = 3; 
+
+int main() {
+     int matriz[SIZE][SIZE];
+       
+
+    cout << "Digite os elementos da matriz " << SIZE << "x" << SIZE << ":" << endl;
+    for (int i = 0; i < SIZE; ++i) {
+        for (int j = 0; j < SIZE; ++j) {
+            cout << "Elemento [" << i << "][" << j << "]: ";
+            cin >> matriz[i][j];
+        }
+    }
+        
+    int soma = 0;
+    for (int i = 0; i < SIZE; ++i) {
+        soma += matriz[i][i];
+        
+    }
+     cout << "A soma dos elementos da diagonal principal da matriz é: " << soma << endl;
+
+        
+    }
+
